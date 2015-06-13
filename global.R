cec450aaea896877e5211c9bbc419aac6acbc346
kisscool62 <kisscool@gmail.com>
2015-06-13 20:10:57 +0200

    ok for go live

---
+++ global.R
@@ -0,0 +1,9 @@
+
+
+dates <- 1965:2015
+countries = c("France", "United.States")
+
+df_ages <- data.frame(
+        min_age = c(26.31, 24.32), 
+        max_age = c(30.33, 31.32), 
+        row.names=countries)
